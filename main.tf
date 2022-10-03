8a334c14514d5e3e0462fcbaf1ba73101a6181ee
Matyas Prokop <mprokop@natilik.com>
2022-10-03 20:31:08 +0100

    Test

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 module "web" {
-  source = "./modules/terraform-vsphere-clus2022-fakeservice-demo"
+  source = "./modules/terraform-vsphere-hashiconf2022-fakeservice-demo"
   demo_vms = {
     name             = "web"
-    quantity         = 0
+    quantity         = 1
     service_tag      = "web"
     service_port     = 9090
     upstream_service = "http://app.service.default.ukdcb.natiliksc.com:9091"
@@ -11,10 +11,10 @@ module "web" {
 }
 
 module "app" {
-  source = "./modules/terraform-vsphere-clus2022-fakeservice-demo"
+  source = "./modules/terraform-vsphere-hashiconf2022-fakeservice-demo"
   demo_vms = {
     name             = "app"
-    quantity         = 0
+    quantity         = 1
     service_tag      = "app"
     service_port     = 9091
     upstream_service = ""
